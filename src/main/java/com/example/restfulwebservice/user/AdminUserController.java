6d80e6f1d1f1bfd0a2f9ea8c2f9590e8cd986d15
KiHwan Yun <drg2524@naver.com>
2023-07-21 21:01:09 +0900

    프로그래밍 필터 제어-개별 사용자 조회

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -1,6 +1,10 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -9,11 +13,12 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-public class UserController {
+@RequestMapping("/admin")
+public class AdminUserController {
     private UserDaoService service;
 
 
-    public UserController(UserDaoService service){
+    public AdminUserController(UserDaoService service){
         this.service = service;
     }
 
@@ -25,35 +30,22 @@ public class UserController {
 
     // GET/users/1 or / users/10 -> String
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){ // 자동으로 int로 처리됨
+    public MappingJacksonValue retrieveUser(@PathVariable int id){ // 자동으로 int로 처리됨
         User user = service.findOne(id);
 
         if(user == null){
             throw new UserNotFoundException(String.format("ID[%s] not found",id));
         }
-        return user;
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id","name","password","ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo",filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
     }
 
-    @PostMapping("/users") // 사용자 추가
-    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
-        User savedUser = service.save(user);
-
-       URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(savedUser.getId())
-                .toUri();
-
-       return ResponseEntity.created(location).build();
-    }
-
-    @DeleteMapping("/users/{id}")
-    public void deleteUser(@PathVariable int id){
-        User user = service.deleteMyId(id);
-
-        if (user == null){
-            throw new UserNotFoundException(String.format("ID[%s] not found",id));
-        }
-    }
     @PutMapping("/users/{id}")
     public User putUser(@PathVariable int id ,@RequestBody User user){
         service.putMyid(id,user);
